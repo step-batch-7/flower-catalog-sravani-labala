7b1e66d2c8b81a0e6f6b850148031cda2bbd930f
Sravani Labala <sravani.labala@gmail.com>
2020-01-31 18:31:00 +0530

    added some more test cases for testing images and pdf's

--- test/testHandler.js
+++ test/testHandler.js
@@ -3,7 +3,7 @@ const { app } = require('../handler');
 const fs = require('fs');
 const sinon = require('sinon');
 
-describe('GET method', function() {
+describe('GET method for pages', function() {
   it('should give the home page of the app for the url /', function(done) {
     request(app.serve.bind(app))
       .get('/')
@@ -26,6 +26,20 @@ describe('GET method', function() {
       .get('/guestBook.html')
       .expect('Content-Type', 'text/html')
       .expect(/submit/)
+      .expect(200, done);
+  });
+  it('should give the abeliophyllum page of the app for the url /abeliophyllum.html', function(done) {
+    request(app.serve.bind(app))
+      .get('/abeliophyllum.html')
+      .expect('Content-Type', 'text/html')
+      .expect(/Abeliophyllum.pdf/)
+      .expect(200, done);
+  });
+  it('should give the guest page of the app for the url /ageratum.html', function(done) {
+    request(app.serve.bind(app))
+      .get('/ageratum.html')
+      .expect('Content-Type', 'text/html')
+      .expect(/Ageratum.pdf/)
       .expect(200, done);
   });
 });
@@ -55,5 +69,71 @@ describe('Method Not Allowed', function() {
     request(app.serve.bind(app))
       .put('/')
       .expect(400, done);
+  });
+});
+
+describe('GET method for css', function() {
+  it('should give the css for the app for flower.css', function(done) {
+    request(app.serve.bind(app))
+      .get('/css/flower.css')
+      .expect('Content-Type', 'text/css')
+      .expect(/#content {/)
+      .expect(200, done);
+  });
+  it('should give the css for the app for guestbook.css', function(done) {
+    request(app.serve.bind(app))
+      .get('/css/guestbook.css')
+      .expect('Content-Type', 'text/css')
+      .expect(/#commentBox {/)
+      .expect(200, done);
+  });
+  it('should give the css for the app for home.css', function(done) {
+    request(app.serve.bind(app))
+      .get('/css/home.css')
+      .expect('Content-Type', 'text/css')
+      .expect(/#homePage {/)
+      .expect(200, done);
+  });
+});
+
+describe('GET method for images', function() {
+  it('should give the image for the app for animated jar', function(done) {
+    request(app.serve.bind(app))
+      .get('/images/animatedBucket.gif')
+      .expect('Content-Type', 'image/gif')
+      .expect(200, done);
+  });
+  it('should give the image for the app for catalog image', function(done) {
+    request(app.serve.bind(app))
+      .get('/images/catalogImage.jpg')
+      .expect('Content-Type', 'image/jpg')
+      .expect(200, done);
+  });
+  it('should give the image for the app for pbase-Abeliophyllum image', function(done) {
+    request(app.serve.bind(app))
+      .get('/images/pbase-Abeliophyllum.jpg')
+      .expect('Content-Type', 'image/jpg')
+      .expect(200, done);
+  });
+  it('should give the image for the app for pbase-agerantum image', function(done) {
+    request(app.serve.bind(app))
+      .get('/images/pbase-agerantum.jpg')
+      .expect('Content-Type', 'image/jpg')
+      .expect(200, done);
+  });
+});
+
+describe('GET method for pdf', function() {
+  it('should give the pdf for the app for Abeliophyllum pdf', function(done) {
+    request(app.serve.bind(app))
+      .get('/pdf/Abeliophyllum.pdf')
+      .expect('Content-Type', 'application/pdf')
+      .expect(200, done);
+  });
+  it('should give the pdf for the app for agerantum pdf', function(done) {
+    request(app.serve.bind(app))
+      .get('/pdf/Ageratum.pdf')
+      .expect('Content-Type', 'application/pdf')
+      .expect(200, done);
   });
 });
